2ceb5f54edd229a9be169af0c0533896c862d88a
AnsonTrang <75271817+AnsonTrang@users.noreply.github.com>
2021-04-23 14:56:14 -0500

    Tweaking the enemy hit movement was a mistake
    It really was, now the HP is off... now I'm increasing the HP.

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,7 +19,7 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
-                { "type": "enemy", "x" : 1000, "y": groundY - 50, "hp": 12},
+                { "type": "enemy", "x" : 1000, "y": groundY - 50, "hp": 50},
                 { "type": "reward", "x": 1500, "y": groundY - 50, "health": 10},
                 { "type": "dodger", "x": 2000, "y": groundY - 50, "hp": 2},
             ]
